e08acbd69f1069a197ba2a66a756c5d7fd758c51
Kimani Kabiria <akabiria@co-opbank.co.ke>
2024-01-18 16:34:11 +0300

    Fixed Header Errors on production build

--- src/components/layout/pages/header.tsx
+++ src/components/layout/pages/header.tsx
@@ -1,7 +1,7 @@
-import clsx from 'clsx';
-import { m } from 'framer-motion';
+import clsx from "clsx";
+import { m } from "framer-motion";
 
-import type { ReactNode } from 'react';
+import type { ReactNode } from "react";
 
 const animation = {
     hide: { x: -32, opacity: 0 },
@@ -23,45 +23,29 @@ interface HeaderProps {
 function Header({
     title,
     description,
-    caption = '',
+    caption = "",
     headerImage = null,
 }: HeaderProps) {
     return (
         <Box p={20}>
             <Stack>
                 {caption && (
-                    <m.Box
-                        initial={animation.hide}
-                        animate={animation.show}
-                        transition={{ delay: 0 }}
-                    >
-                        <span
-                            className='mb-1 block text-lg font-extrabold capitalize leading-none md:mb-0 md:text-2xl'
-                        >
+                    <Box>
+                        <span className="mb-1 block text-lg font-extrabold capitalize leading-none md:mb-0 md:text-2xl">
                             {caption}
                         </span>
-                    </m.Box>
+                    </Box>
                 )}
-                <m.Box
-                    initial={animation.hide}
-                    animate={animation.show}
-                    transition={{ delay: 0.1 }}
-                >
-                    <h1
-                        className='text-[2.5rem] font-bold leading-tight md:text-7xl md:leading-snug'
-                    >
+                <Box>
+                    <h1 className="text-[2.5rem] font-bold leading-tight md:text-7xl md:leading-snug">
                         {title}
                     </h1>
-                </m.Box>
-                <m.Box
-                    initial={animation.hide}
-                    animate={animation.show}
-                    transition={{ delay: 0.2 }}
-                >
-                <Text fontSize={"xl"} color={""} >
-                    {description}
-                </Text>
-                </m.Box>
+                </Box>
+                <Box>
+                    <Text fontSize={"xl"} color={""}>
+                        {description}
+                    </Text>
+                </Box>
             </Stack>
         </Box>
     );
